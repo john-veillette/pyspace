3e8d23f6d2a590c13de517f09e06c1029256d74d
adityapb <adityapb1546@gmail.com>
2016-03-03 21:34:11 +0530

    Convert tabs to spaces

--- src/pyspace.hpp
+++ src/pyspace.hpp
@@ -14,9 +14,9 @@ class SimConfig;
 
 struct ThreeTuple
 {
-	double x;
-	double y;
-	double z;
+    double x;
+    double y;
+    double z;
 };
 
 typedef ThreeTuple Position;
@@ -24,53 +24,53 @@ typedef ThreeTuple Velocity;
 
 struct SimConfig
 {
-	double G;
-	double step_size;
-	void write_to_file(std::string filename);
+    double G;
+    double step_size;
+    void write_to_file(std::string filename);
 };
 
 class Simulator
 {
 private:
-	//Data Structures
-	std::vector<SimObject> obj_list;
-	SimConfig config;
+    //Data Structures
+    std::vector<SimObject> obj_list;
+    SimConfig config;
 public:
-	Simulator();
-	Simulator(SimConfig);
-	Simulator(std::string filename);
-	~Simulator();
-	
-	int add_object(SimObject);
-	std::vector<SimObject> &get_objects();
-	int delete_object(int);
+    Simulator();
+    Simulator(SimConfig);
+    Simulator(std::string filename);
+    ~Simulator();
+    
+    int add_object(SimObject);
+    std::vector<SimObject> &get_objects();
+    int delete_object(int);
 
-	void write_to_file(std::string filename);
-	int get_status();
-	void start_simulation(double);
-	void abort();
+    void write_to_file(std::string filename);
+    int get_status();
+    void start_simulation(double);
+    void abort();
 
 };
 
 class SimObject
 {
-	friend class Simulator;
-	
+    friend class Simulator;
+    
 private:
-	//Data Structures
-	static int object_id;
-	double mass;
-	double radius;
-	Position position;
-	Velocity velocity;
+    //Data Structures
+    static int object_id;
+    double mass;
+    double radius;
+    Position position;
+    Velocity velocity;
 public:
-	SimObject(double mass, double radius);
-	~SimObject();
-	
-	void set_position(Position);
-	void set_velocity(Velocity);
+    SimObject(double mass, double radius);
+    ~SimObject();
+    
+    void set_position(Position);
+    void set_velocity(Velocity);
  
-	Velocity get_velocity();
-	Position get_position();
+    Velocity get_velocity();
+    Position get_position();
 };
 
