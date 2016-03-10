b89126dfbf623036b7cb8945988bcd8ebc4f82b1
adityapb <adityapb1546@gmail.com>
2016-03-10 13:45:46 +0530

    Add warning to PlanetArray

--- pyspace/planet.pyx
+++ pyspace/planet.pyx
@@ -12,5 +12,14 @@ cdef class PlanetArray:
 
     cpdef add_planet(self, double mass, double radius, Vector init_pos,
             Vector init_vel, int planet_id):
+        """Adds a planet to PlanetArray
+
+        Note
+        ----
+        **WARNING**
+        The Vector's init_pos and init_vel are being passed around as references.
+        As a consequence, if the Vector's passed to this function go out of scope,
+        the simulation will return garbage values.
+        """
         self._add_planet(mass, radius, init_pos, init_vel, planet_id)
 
