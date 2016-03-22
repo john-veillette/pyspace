14d890733140069c15c97e9bf94f1474b548a3fb
adityapb <adityapb1546@gmail.com>
2016-03-22 21:48:15 +0530

    Add test for custom vtk dumps

--- pyspace/tests/test_vtk.py
+++ pyspace/tests/test_vtk.py
@@ -1,11 +1,20 @@
 from pyspace.utils import dump_vtk
 import numpy
 from pyspace.planet import PlanetArray
+from pyspace.simulator import BruteForceSimulator
+import unittest
 
-def test_vtk_dump():
-    x, y, z = numpy.mgrid[0:1:0.04, 0:1:0.04, 0:1:0.04]
-    x = x.ravel(); y = y.ravel(); z = z.ravel()
-    v_x = numpy.ones_like(x)
-    pa = PlanetArray(x=x, y=y, z=z, v_x=v_x)
-    dump_vtk(pa, "points")
+class TestVTK(unittest.TestCase):
+    def setUp(self):
+        x, y, z = numpy.mgrid[0:1:0.04, 0:1:0.04, 0:1:0.04]
+        self.x = x.ravel(); self.y = y.ravel(); self.z = z.ravel()
+        self.pa = PlanetArray(x=self.x, y=self.y, z=self.z)
+
+    def test_vtk_dump(self):
+        dump_vtk(self.pa, "points")
+
+    def test_simulator_custom_vtk_dump(self):
+        sim = BruteForceSimulator(self.pa, G = 1, dt = 1, sim_name = "test_vtk")
+        sim.set_data(a_x = 'a_x')
+        sim.simulate(1, dump_output = True)
 
