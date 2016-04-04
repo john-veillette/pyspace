affbd364a8aa965883749802cd2625c59483af9a
adityapb <adityapb1546@gmail.com>
2016-04-04 18:56:00 +0530

    Fix Benchmark base class

--- benchmark/benchmark/benchmark.py
+++ benchmark/benchmark/benchmark.py
@@ -6,8 +6,8 @@ class Benchmark(object):
     def __init__(self):
         x, y, z = numpy.mgrid[0:500:25j, 0:500:25j, 0:500:25j]
         self.x = x.ravel()
-        self.y = x.ravel()
-        self.z = x.ravel()
+        self.y = y.ravel()
+        self.z = z.ravel()
         self.pa = PlanetArray(x=self.x, y=self.y, z=self.z)
 
 
