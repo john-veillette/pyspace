7fc2083b25523d77acab5e56006f4cf5c44d80da
Aditya Bhosale <adityapb1546@gmail.com>
2016-04-11 23:57:45 +0530

    Fix examples

--- examples/brute_force_random.py
+++ examples/brute_force_random.py
@@ -9,7 +9,7 @@ z = numpy.random.random_integers(0,500,500)
 
 pa = PlanetArray(x, y, z)
 
-sim = BruteForceSimulator(pa, 1, 1, "random")
+sim = BruteForceSimulator(pa, 1, 1, sim_name = "random")
 
 sim.simulate(1000, dump_output = True)
 
--- examples/brute_force_two_planets.py
+++ examples/brute_force_two_planets.py
@@ -13,7 +13,7 @@ v_y = numpy.array([0,(1000/100)**0.5])
 
 pa = PlanetArray(x, y, z, v_y=v_y, m=m)
 
-sim = BruteForceSimulator(pa, 1, 1, "two_planets")
+sim = BruteForceSimulator(pa, 1, 1, sim_name = "two_planets")
 
 sim.simulate(2500, dump_output = True)
 
