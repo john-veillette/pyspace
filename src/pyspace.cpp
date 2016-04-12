3eee0a55dab3ef1d0b3f44ff1e14060309581d9e
adityapb <adityapb1546@gmail.com>
2016-04-12 12:47:26 +0530

    Update gpu framework

--- src/pyspace.cpp
+++ src/pyspace.cpp
@@ -325,11 +325,4 @@ void brute_force_update(double* x, double* y, double* z,
     delete[] z_old;
 }
 
-void brute_force_gpu_update(double* x, double* y, double* z,
-        double* v_x, double* v_y, double* v_z,
-        double* a_x, double* a_y, double* a_z,
-        double* m, double G, double dt, int num_planets, double eps)
-{
-    //Call gpu kernels here
-}
 
