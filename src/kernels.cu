a3b92515c0cb56c77f746fe107beaef11fe3cc17
adityapb <adityapb1546@gmail.com>
2016-05-03 00:50:06 +0530

    512 threads per block not supported by some GPU's

--- src/kernels.cu
+++ src/kernels.cu
@@ -135,8 +135,8 @@ void brute_force_gpu_update(double* x, double* y, double* z,
         exit(0);
     }
 
-    int num_blocks = ceil(num_planets/512)+1;
-    brute_force_kernel<<<num_blocks, 512>>>(dev_x, dev_y, dev_z,
+    int num_blocks = ceil(num_planets/256)+1;
+    brute_force_kernel<<<num_blocks, 256>>>(dev_x, dev_y, dev_z,
             dev_x_old, dev_y_old, dev_z_old,
             dev_v_x, dev_v_y, dev_v_z,
             dev_a_x, dev_a_y, dev_a_z,
